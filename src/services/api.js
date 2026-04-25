3094a6af4756c4e78ec890592b5a043fcd685bce
Nikhilmukheja19 <128170642+Nikhilmukheja19@users.noreply.github.com>
2026-04-25 17:03:06 +0530

    removed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://vexora-backend-1.onrender.com/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
