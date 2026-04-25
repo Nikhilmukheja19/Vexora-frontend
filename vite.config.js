7d10a5e058b3b5dc23168fb77d9c287d621ffe32
Nikhilmukheja19 <128170642+Nikhilmukheja19@users.noreply.github.com>
2026-04-25 15:59:35 +0530

    url updated

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'https://vexora-backend-1.onrender.com',
+      "/api": {
+        target: "https://vexora-backend-1.onrender.com",
         changeOrigin: true,
       },
-      '/uploads': {
-        target: 'https://vexora-backend-1.onrender.com',
+      "/uploads": {
+        target: "https://vexora-backend-1.onrender.com",
         changeOrigin: true,
       },
     },
   },
-})
+});
